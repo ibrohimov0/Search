c13c97ebcd0d635aa759b51953ecc7ea07606bf7
Abdulloh Ibrohimov <109219970+ibrohimov0@users.noreply.github.com>
2023-02-16 17:11:12 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-const Loading = document.getElementById("Loading")
-const Form = document.getElementById("Form")
+const fs = require("fs")
 
 async function Get(name, country) {
     let a = null
@@ -7,8 +6,9 @@ async function Get(name, country) {
         `http://universities.hipolabs.com/search?name=${name}&country=${country}`)
     .then(res => res.json())
     .then(res => a = res)
-    
+    fs.writeFileSync("./index.html",``, (req,res)=>{
+        res.end()
+    })
 }
 console.log(Get("a", "turkey"));
-
 // http://universities.hipolabs.com/search?name=${name}&country=${country}
